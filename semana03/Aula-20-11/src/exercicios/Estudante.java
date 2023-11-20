cfd6a27c716b37680b5db5f5426622c300b41bbf
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-20 14:47:24 -0300

    att classe Estudante

--- semana03/Aula-20-11/src/exercicios/Estudante.java
+++ semana03/Aula-20-11/src/exercicios/Estudante.java
@@ -5,31 +5,36 @@ import java.util.Scanner;
 import java.text.SimpleDateFormat;
 
 public class Estudante {
-    public static void main(String[] args) {
-        Date dataNascimento;
-        String nome;
-        String CPF;
-        Scanner sc = new Scanner(System.in);
+    private Date dataNascimento;
+    private String nome;
+    private String CPF;
+    private Scanner sc = new Scanner(System.in);
 
-        // lendo dados do estudante
+    public Estudante(String nome, String CPF, String dataNascimento){
+        this.nome = nome;
+        this.CPF = CPF;
+        this.dataNascimento = stringToDate(dataNascimento);
+    }
+
+    public Estudante(){
         System.out.print("Digite o nome do estudante: ");
-        nome = sc.nextLine();
+        this.nome = sc.nextLine();
         System.out.print("Digite o CPF do estudante: ");
-        CPF = sc.nextLine();
+        this.CPF = sc.nextLine();
         System.out.print("Digite a data de nascimento do estudante: ");
-        dataNascimento = stringToDate(sc.nextLine());
+        this.dataNascimento = stringToDate(sc.nextLine());
+    }
 
-        // imprimir dados do estudante
-        System.out.println("Nome: " + nome);
-        System.out.println("CPF: " + CPF);
-        System.out.println("Data de nascimento: " + dataNascimento);
+    public static void main(String[] args) {
+        Estudante e1 = new Estudante();
+        Estudante e2 = new Estudante("João", "123456789-00", "01/01/2000");
 
-        sc.close();
-        
+        e1.printEstudante();
+        e2.printEstudante();
     }
 
     // converter uma string no formato dd/MM/yyyy para um objeto Date
-    public static Date stringToDate(String data){
+    private static Date stringToDate(String data){
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Date dataNascimento = new Date();
 
@@ -40,6 +45,33 @@ public class Estudante {
         }
         return dataNascimento;
     }
+
+    public void printEstudante(){
+        System.out.println("\nNome: " + this.nome);
+        System.out.println("CPF: " + this.CPF);
+        System.out.println("Data de nascimento: " + this.dataNascimento);
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public String getCPF(){
+        return this.CPF;
+    }
+    public Date getDataNascimento(){
+        return this.dataNascimento;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setCPF(String CPF){
+        this.CPF = CPF;
+    }
+    public void setDataNascimento(String dataNascimento){
+        this.dataNascimento = stringToDate(dataNascimento);
+    }
+
 
 
     
