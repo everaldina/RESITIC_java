7cf91ae84be3eb39b21d930600031ed9487fa558
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-11-20 11:55:58 -0300

    add exercicicio soma de inteiros

---
+++ semana03/Aula-20-11/src/exercicios/SomaInteiros.java
@@ -0,0 +1,11 @@
+package exercicios;
+public class SomaInteiros {
+    public static void main(String[] args) throws Exception {
+        int a = 10, b =  5;
+        System.out.println(+ a + " + " + b + " = " + soma(a, b));
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
